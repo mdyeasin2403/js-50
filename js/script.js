d539ebfdb9902393d24e1ad5a5f7045a13edab13
mdyeasin2403 <168212097+mdyeasin2403@users.noreply.github.com>
2024-09-29 18:53:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,246 @@
+//ex1
+function textChange(){
+	document.getElementById('ban').innerHTML="I am Robboto";
+}
+//ex2
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+//ex3
+function changeCss(){
+	document.getElementById('cc').style.color="green";
+}
+//ex4
+function textShow(){
+	document.getElementById('sh').style.display="block";
+}
+
+function textHide(){
+	document.getElementById('sh').style.display="none";
+}
+//ex5
+function smilePic(){
+	document.getElementById('sp').src="images/sm.jpg";
+}
+
+function sadPic(){
+	document.getElementById('sp').src="images/sd.jpg";
+}
+//ex6
+function changeFs(){
+	document.getElementById('fs').style.fontSize="40px";
+}
+//ex7
+function showNumber(){
+	document.write(5+6);
+}
+//ex8
+function winShow(){
+	window.alert("Are you sure that you want to enter?");
+}
+//ex9
+function showCon(){
+	console.log(5 + 6);
+}
+//ex10
+let x, y;
+x = 5;
+y = 6;
+document.getElementById('in').innerHTML=(x + y);
+//ex11
+document.getElementById('jd').innerHTML="John" + " " + "Doe";
+//ex12
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+document.getElementById("yjd").innerHTML =
+pi + "<br>" + person + "<br>" + answer;
+//ex13
+let tea = 5 + "2" + 3;
+document.getElementById("st").innerHTML = tea;
+//ex14
+let a = 5;
+let b = 2;
+let c = a + b;
+document.getElementById("ao").innerHTML = c;
+//ex15
+let d = 5;
+let e = 2;
+let f = d * e;
+document.getElementById("mo").innerHTML = f;
+
+//ex16
+let g = 3;
+let h = (100 + 50) * g;
+document.getElementById("j1").innerHTML = h;
+
+//ex17
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+document.getElementById("j2").innerHTML = "Is A less than B? " + result;
+
+//ex18
+let text4 = "John";
+let text5 = "Doe";
+let text6 = text4 + " " + text5;
+document.getElementById("j3").innerHTML = text6;
+
+//ex19
+let ami = 5 + 5;
+let tumi= "5" + 5;
+let she = "Hello" + 5;
+document.getElementById("j4").innerHTML =
+ami + "<br>" + tumi + "<br>" + she;
+
+//ex20
+let i = 5;
+let j = 2;
+let k = i + j;
+document.getElementById("j5").innerHTML = k;
+
+//ex21
+let l = 5;
+let m = 2;
+let n = l - m;
+document.getElementById("j6").innerHTML = n;
+
+//example22
+let mo = 5;
+let po = 2;
+let zo = mo / po;
+document.getElementById("mpz").innerHTML = zo;
+
+//example23
+let ai = 5;
+let bi = 2;
+let ci = ai % bi;
+document.getElementById("rj").innerHTML = ci;
+
+
+//example24
+function changeBots(){
+	document.getElementById('cbbs').style.borderBottom = '2px  dashed red';
+}
+
+//example25
+function changeBotw(){
+	document.getElementById('cbbw').style.borderBottom = "2px dashed red";
+}
+//example26
+function changeBorl(){
+	document.getElementById('cbbl').style.borderLeft = " 5px solid";
+}
+//example27
+function changeBorls(){
+	document.getElementById('cbbls').style.borderLeftStyle = " dashed";
+}
+//example28
+function changeBorr(){
+	document.getElementById('cbbrrr').style.borderRight = "2px solid pink";
+}
+//example29
+function changeBorrrs(){
+	document.getElementById('cbbrrrs').style.borderRightStyle = " dashed";
+}
+//example30
+function changeBoxs(){
+	document.getElementById('cboxs').style.boxShadow = "10px 20px 30px green";
+}
+
+//example31
+function myFlow() {
+  document.getElementById("tf").style.direction = "rtl";
+}
+//example32
+function myDisplay() {
+  document.getElementById("dp").style.display = "none";
+}
+//example33
+function myFunction() {
+  document.getElementById("ff").style.fontFamily = "Volkhov, serif";
+}
+//example34
+function myFont() {
+  document.getElementById("kj").style.fontStyle = "bold";
+}
+//example35
+function myFunctionv() {
+  document.getElementById("fv").style.fontVariant = "small-caps";
+}
+//example36
+function myFunctionw() {
+  document.getElementById("fw").style.fontWeight = "900";
+}
+
+//example37
+function myFunctionb() {
+  document.getElementById("myBtn").style.height = "50px";
+}
+//example38
+
+function myFunctionbx() {
+  document.getElementById("box").style.justifyContent = "space-between";
+}
+//example39
+function myFunctionl() {
+  document.getElementById("myList").style.listStyle = "decimal inside";
+}
+//example40
+
+function myFunctionlp() {
+  document.getElementById("myUL").style.listStylePosition = "inside";
+}
+
+//example41
+
+function myFunctionpr() {
+  document.getElementById("myULI").style.listStyleType = "upper-roman";
+}
+
+//example42
+function myFunctionm() {
+  document.getElementById("mr").style.margin = "50px 10px 20px 30px";
+}
+
+//example43
+function myFunctionmb() {
+  document.getElementById("mb").style.marginBottom = "50px";
+}
+
+//example44
+function myFunctionml() {
+  document.getElementById("ml").style.marginLeft = "50px";
+}
+
+
+//example45
+function myFunctionmrr() {
+  document.getElementById("mrr").style.marginRight = "50px";
+}
+//example46
+function myFunctionmt() {
+  document.getElementById("mt").style.marginTop = "50px";
+}
+
+//example47
+function myFunctionmh() {
+  document.getElementById("mh").style.maxHeight = "100px";
+}
+
+//example48
+function myFunctiontd() {
+  document.getElementById("td").style.textDecoration = "underline overline";
+}
+
+//example49
+function changeColor() {
+  am.style.textDecorationColor = "red";
+}
+
+//exampl50
+function myFunctiontt() {
+  document.getElementById("tut").style.textTransform = "uppercase";
+}
+
+
